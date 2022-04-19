1c76b2596857458e7c87b8b9ae54e03f3a6e59e7
Pingvu <nhlpool27@gmail.com>
2022-04-19 14:07:31 +0200

    1233

--- dropdown.js
+++ dropdown.js
@@ -1,8 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+const nav = document.getElementById("nav");
+const button = document.getElementById("burgerButton");
+
+button.addEventListener("click", burgermenu);
+
+function burgermenu() {
+    if (nav.style.display === "grid") {
+      nav.style.display = "none";
     } else {
-      x.style.display = "block";
+      nav.style.display = "grid";
     }
   }
